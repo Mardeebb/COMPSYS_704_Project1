3d7d787afaa411a70eeb4014942d8fe33a6cc15a
Tommy Chen <126313171+xche529@users.noreply.github.com>
2025-09-10 16:01:00 +1200

    feat: workdone

---
+++ Project_1_Copy/src/org/compsys704/TurnTable.java
@@ -0,0 +1,14 @@
+package org.compsys704;
+
+public class TurnTable {
+	
+	public int[] RT = {0, 0, 1, 0, 0, 5};
+	public int RT_PS = 0;
+	public boolean bottleAtPos5;
+	public boolean tableAlignedWithSensor;
+	public boolean capOnBottleAtPos1;
+
+	public TurnTable(){
+	}
+
+}
